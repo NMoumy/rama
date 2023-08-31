61cc639192322626f21866c67db6fc97af343bf0
Moumy Ndiaye <moumyndiaye@outlook.com>
2023-08-31 12:59:59 -0400

    Début de la page de profil d'un utilisateur

--- src/composants/Appli.jsx
+++ src/composants/Appli.jsx
@@ -1,12 +1,14 @@
 import './Appli.scss';
 // import PageAccueil from './PageAccueil';
-import PageConnexion from './PageConnexion';
+// import PageConnexion from './PageConnexion';
+import PageProfil from './PageProfil';
 
 export default function Appli() {
   return (
     <div className="Appli">
       {/* <PageAccueil/> */}
-      <PageConnexion/>
+      {/* <PageConnexion/> */}
+      <PageProfil/>
     </div>
   );
 }
--- src/composants/PageProfil.jsx
+++ src/composants/PageProfil.jsx
@@ -3,6 +3,31 @@ import './PageProfil.scss';
 export default function PageProfil() {
   return (
     <div className="PageProfil">
+
+      <div className='info-profil'>
+
+        <div className="nom-util">
+          <span className="photo-profil"></span>
+          <h2>Le Nom </h2>
+        </div>
+
+        <div className='status'>
+          <div className='abonnement'>
+            <p>Abonnement</p>
+            <p>124678</p>
+          </div>
+          <div className='abonnées'>
+            <p>Abonnées</p>
+            <p>3214s</p>
+          </div>
+          <div className='aime'>
+            <p>Aime</p>
+            <p>123</p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
